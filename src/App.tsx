88d0429743c6fd7e63302c99a49483f7236320d5
slee6390 <seoyeon_lee@berkeley.edu>
2025-10-09 14:57:10 -0700

    Update repository name from sunny-outlook-portfolio to portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/sunny-outlook-portfolio">
+      <BrowserRouter basename="/portfolio">
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
